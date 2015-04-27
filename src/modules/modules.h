c43e19374d69660b2754f1262f5f6781365212c8
hexxert <shadel540@hotmail.com>
2015-04-27 18:50:26 +0100

    function pointers added

--- src/modules/modules.h
+++ src/modules/modules.h
@@ -15,33 +15,54 @@
 		/* the next table is related to the auxiliary data
 		 * and is called on layer creation
 		 */
-		void * plt_aux_data_func[	plt_2d_line_aux, 
-									plt_3d_line_aux,
-									plt_mesh_aux,
-									plt_surf_aux
-								 ];
-		
+		/* declare pointer to function of this type 
+		 * functions are of the form void function(plot_t * plt, int32_t layer)
+		 */
+		typedef void (* plt_aux_data_func_ptr)(plot_t *, int32_t);
+		 
+		plt_aux_data_func_ptr[PL_NUM_PLOT_TYPES] = {	
+														plt_2d_line_aux, 
+														plt_3d_line_aux,
+														plt_mesh_aux,
+														plt_surf_aux
+													};
+								 
 		/* validate requirements on new layer */
-		void * plt_val_req_func[	plt_2d_line_req,
-									plt_3d_line_req,
-									plt_mesh_req,
-									plt_surf_req
-								];
+		typedef void (* plt_val_req_func_ptr)(plot_t *, double *, double *, double *, int32_t);
+		plt_val_req_func_ptr[PL_NUM_PLOT_TYPES] = {	
+														plt_2d_line_req,
+														plt_3d_line_req,
+														plt_mesh_req,
+														plt_surf_req
+													};
 							
 		/* draw functions */
-		void * plt_draw_func[	plt_2d_line_draw,
-								plt_3d_line_draw,
-								plt_mesh_draw,
-								plt_surf_req
-							];
+		typedef void (* plt_draw_func_ptr)(plot_t *, int32_t, int32_t);
+		plt_draw_func_ptr[PL_NUM_PLOT_TYPES] = {	
+													plt_2d_line_draw,
+													plt_3d_line_draw,
+													plt_mesh_draw,
+													plt_surf_req
+												};
 		
 		/* functions to be called on layer property change */
-		void * plt_layer_prop[	plt_2d_line_layer,
-								plt_3d_line_layer,
-								plt_mesh_layer,
-								plt_surf_layer,
+		typedef void (* plt_set_layer_prop_func_ptr)(int32_t, int32_t);
 		
-							];
+		plt_set_layer_prop_func_ptr[PL_NUM_PLOT_TYPES] = {
+															plt_2d_line_layer,
+															plt_3d_line_layer,
+															plt_mesh_layer,
+															plt_surf_layer,
+														};
+		/* free plot layer */
+		typedef void (*plt_free_aux_data_func_ptr);
+		
+		plt_free_aux_data_func_ptr[PL_NUM_PLOT_TYPES] = {
+															plt_2d_line_free_aux,
+															plt_3d_line_free_aux,
+															plt_mesh_free_aux,
+															plt_surf_free_aux
+														};
 		
 		
 
