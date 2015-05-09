be1ac737431e35b0178a9f845eeaaa0ae6fdee6d
hexxert <shadel540@hotmail.com>
2015-05-09 15:33:34 +0100

    color conversion

--- src/base/common.c
+++ src/base/common.c
@@ -10,6 +10,55 @@ void plt_adjust_mapminmax(const plot_t * plt, const double * x, const double * y
 	
 }
 
+/* convert hsv into RGB color 
+ * format
+ *
+ * requires h in 0..360
+ *			s in 0..1
+ *			v in 0..1
+ */
+uint32_t plt_get_hsv_color(float h, float s, float v){
+	float r = 0.0f, g = 0.0f, b = 0.0f;
+	float hl = 0.0f, c  = 0.0f, x = 0.0f, m = 0.0f;
+	
+	h = fmodf(h , 360.0f);
+	s = s - floorf(s);
+	v = v - floorfS(v);
+	
+	/* chroma */
+	c = v * s;
+	
+	/* hue line */
+	hl = h/60.0f;
+	
+	x = c * (1 - abs( fmodf(hl, 2) - 1));
+		
+	switch(floor(hl))
+	{
+		case 0:	r = c; 	g = x; break;
+		case 1: r = x; 	g = c; break;
+		case 2: g = c; 	b = x; break;
+		case 3: g = x; 	b = c; break;
+		case 4: r = x; 	b = c; break;
+		case 5: r = c; 	b = x; break;
+		default: r = 0; g = 0; b = 0; break;
+	}
+	
+	r += v - c;
+	g += v - c;
+	b += v - c;
+	
+	return ( ((uint32_t)(r * 255) << 16 ) | 
+			 ((uint32_t)(g * 255) <<  8 ) |
+			  (uint32_t)(b * 255) 
+			);
+			  
+}
+
+void plt_get_hue_color( int32_t color , int32_t num_colors ){
+	plt_get_hcl_color(float h, 0.65f, 1.0f);
+}
+
 
 void draw_2d_axes(){
 
